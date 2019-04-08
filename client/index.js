e722974fde5954f32ff4248297d2a1e6c2e9008a
Sam-Bartos <sambartos@gmail.com>
2019-04-08 14:15:40 +1200

    redux added

--- client/index.js
+++ client/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+
 import App from './components/App'
+import reducers from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(reduxThunk)
+))
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
-        <App/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
       document.getElementById('app')
     );
   })
